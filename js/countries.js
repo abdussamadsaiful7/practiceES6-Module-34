cafe0cfbd6b03e5ae786d24f223f499ee9ae3c77
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-25 17:48:39 +0600

    added ES6 code for country details of js

--- js/countries.js
+++ js/countries.js
@@ -9,13 +9,14 @@ const displayCountries = countries =>{
     // console.log(country)
 //     for(const country of countries){}
     countries.forEach(country => {
-        console.log(country);
+        console.log(country.cca2);
         const countryDiv = document.createElement('div');
         countryDiv.classList.add('country');
         countryDiv.innerHTML =`
             <div>
                 <h3>Name: ${country.name.common}</h3>
                 <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</p>
+                <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
             </div>
 
 
@@ -24,10 +25,26 @@ const displayCountries = countries =>{
         countriesContainer.appendChild(countryDiv);
 
      });   
-
-
-
 }
+
+const loadCountryDetails = code =>{
+    //https://restcountries.com/v3.1/alpha/{code}
+    const url =`https://restcountries.com/v3.1/alpha/${code}`
+    //console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>  showCountryDetail(data[0]))
+}
+
+const showCountryDetail = country =>{
+    console.log(country);
+    const detailContainer= document.getElementById('country-detail');
+    detailContainer.innerHTML =`
+      <h3>Name: ${country.name.common}</h3>
+      <img src="${country.flags.png}">
+    `
+}
+
 
 
 loadCountries();
