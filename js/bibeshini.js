b4a95fad07ee001b5d93905732f21aba5489ad13
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-25 15:43:47 +0600

    added js code of ES6

--- js/bibeshini.js
+++ js/bibeshini.js
@@ -7,9 +7,14 @@ const loadUser = () =>{
 const displayUser = user =>{
     const genderTag = document.getElementById('gender');
     genderTag.innerHTML = user.results[0].gender;
+
     const nameTag = document.getElementById('name');
-    nameTag.innerHTML = user.results[0].name.first + ' ' + user.results[0].name.last; 
-   console.log(user.results[0].name)
+    nameTag.innerHTML = user.results[0].name.title + ' '+ user.results[0].name.first + ' ' + user.results[0].name.last; 
+
+    const locationTag = document.getElementById('location');
+    locationTag.innerHTML = user.results[0].location.country;
+
+   console.log(user.results[0].picture.thumbnail);
 }
 
 loadUser()
